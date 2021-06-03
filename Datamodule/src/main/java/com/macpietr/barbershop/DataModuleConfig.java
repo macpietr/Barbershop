bb5e6a502e2cf7b77ec0cc64cd0e1cfc989e3a7b
macpietr <(Ferrari575)>
2021-06-03 10:37:18 +0200

    rm unnecessary class

--- Datamodule/src/main/java/com/macpietr/barbershop/DataModuleConfig.java
+++
@@ -1,9 +0,0 @@
-package com.macpietr.barbershop;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:application.properties")
-public class DataModuleConfig {
-}
