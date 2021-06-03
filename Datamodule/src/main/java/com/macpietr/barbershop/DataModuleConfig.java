b3b8a1daaa60e8ec48f8ba2f0dcf79a2d914a2cb
macpietr <(Ferrari575)>
2021-06-03 10:24:59 +0200

    Establish multi-module project

---
+++ Datamodule/src/main/java/com/macpietr/barbershop/DataModuleConfig.java
@@ -0,0 +1,9 @@
+package com.macpietr.barbershop;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class DataModuleConfig {
+}
