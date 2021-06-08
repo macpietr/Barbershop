a9409126262c36123fba2d53d9982c74c21c608d
macpietr <(Ferrari575)>
2021-06-08 22:39:07 +0200

    Use properties from Common module

--- Application/src/main/java/com/macpietr/barbershop/BarbershopApplication.java
+++ Application/src/main/java/com/macpietr/barbershop/BarbershopApplication.java
@@ -2,9 +2,11 @@ package com.macpietr.barbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active}.properties")
 public class BarbershopApplication {
 
     public static void main(String[] args) {
