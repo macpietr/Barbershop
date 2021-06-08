b0915446f4d2e13e7e6057b26f508145a2fede48
macpietr <(Ferrari575)>
2021-06-08 22:39:45 +0200

    Use properties form Common module

--- Servicemodule/src/test/java/com/macpietr/barbershop/TestConfigurationClass.java
+++ Servicemodule/src/test/java/com/macpietr/barbershop/TestConfigurationClass.java
@@ -1,7 +1,8 @@
 package com.macpietr.barbershop;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-dev.properties")
 public class TestConfigurationClass {
 }
