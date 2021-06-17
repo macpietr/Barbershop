25affbba01091e9c0c7d93e41294298ab0f5c78a
macpietr <(Ferrari575)>
2021-06-17 19:52:23 +0200

    add adduser and list users

--- Webmodule/src/main/java/com/macpietr/barbershop/controller/IndexContorller.java
+++ Webmodule/src/main/java/com/macpietr/barbershop/controller/IndexContorller.java
@@ -1,10 +1,13 @@
 package com.macpietr.barbershop.controller;
 
+import com.macpietr.barbershop.model.User;
 import com.macpietr.barbershop.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
@@ -22,6 +25,18 @@ public class IndexContorller {
         model.addAttribute("user", userService.findUserById(1L));
         model.addAttribute("users", userService.findAllUsers());
         return "user";
+    }
+
+    @GetMapping("/adduser")
+    public String addUser(Model model){
+        model.addAttribute("newUser", new User());
+        return "adduser";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user){
+        userService.saveUser(user);
+        return "redirect:/adduser";
     }
 
     @GetMapping({"/login.html","login"})
