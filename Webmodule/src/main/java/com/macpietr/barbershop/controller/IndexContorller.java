b5082be13e543e9a4b3e017b70446ebb9e45d99d
macpietr <(Ferrari575)>
2021-06-17 19:43:49 +0200

    Print user on page

--- Webmodule/src/main/java/com/macpietr/barbershop/controller/IndexContorller.java
+++ Webmodule/src/main/java/com/macpietr/barbershop/controller/IndexContorller.java
@@ -1,10 +1,16 @@
 package com.macpietr.barbershop.controller;
 
+import com.macpietr.barbershop.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class IndexContorller {
+
+    UserService userService;
 
     @GetMapping({"","/","/index","/index.html"})
     public String index(){
@@ -12,7 +18,11 @@ public class IndexContorller {
     }
 
     @GetMapping("/user")
-    public String user(){
+    public String user(Model model){
+        model.addAttribute("user", userService.findUserById(1L));
         return "user";
     }
+
+    @GetMapping({"/login.html","login"})
+    public String login(){ return "login";}
 }
