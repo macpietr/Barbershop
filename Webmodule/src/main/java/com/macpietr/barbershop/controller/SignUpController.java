9bea8ef29786a1192b25f050822b5e2b99f9f330
macpietr <mac.pietruszewski@gmail.com>
2021-06-24 18:14:51 +0200

    create form, controller and logic for signup

--- Webmodule/src/main/java/com/macpietr/barbershop/controller/SignUpController.java
+++ Webmodule/src/main/java/com/macpietr/barbershop/controller/SignUpController.java
@@ -1,2 +1,37 @@
-package com.macpietr.barbershop.controller;public class SignUpController {
+package com.macpietr.barbershop.controller;
+
+import com.macpietr.barbershop.model.User;
+import com.macpietr.barbershop.service.BarberService;
+import com.macpietr.barbershop.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
+public class SignUpController {
+
+    private UserService userService;
+    private BarberService barberService;
+
+    @GetMapping("/signup")
+    public String goToSignUpPage(Model model){
+        model.addAttribute("newUser",new User());
+        return "signup";
+    }
+
+    @PostMapping("/signUp")
+    public String signUp(@ModelAttribute User user){
+        if(userService.findUserByEmail(user.getEmail())==null
+                && barberService.findBarberByEmail(user.getEmail())==null){
+            user.setRole("ROLE_USER");
+            userService.saveUser(user);
+            return "redirect:/login";
+        }else {
+            return "redirect:/signup?error";
+        }
+    }
 }
