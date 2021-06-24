5d29eee9ff8e3fd4c82b83c7030d17b2e150457d
macpietr <mac.pietruszewski@gmail.com>
2021-06-25 01:02:33 +0200

    add confirmed field and methods for confirmationUrl

--- Servicemodule/src/main/java/com/macpietr/barbershop/service/UserService.java
+++ Servicemodule/src/main/java/com/macpietr/barbershop/service/UserService.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface UserService {
     User findUserById(Long id);
     User findUserByEmail(String email);
+    User findUserByConfirmationUrl(String confirmationUrl);
     List<User> findAllUsers();
     void saveUser(User user);
     void saveAllUsers(List<User> users);
