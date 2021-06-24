da4109dbc708da1ea1e4f8f2f59ea7532c9f7fd7
macpietr <mac.pietruszewski@gmail.com>
2021-06-25 01:06:15 +0200

    introduce confirmation mechanism. I should clean the code

--- Webmodule/src/main/java/com/macpietr/barbershop/controller/SignUpController.java
+++ Webmodule/src/main/java/com/macpietr/barbershop/controller/SignUpController.java
@@ -1,5 +1,7 @@
 package com.macpietr.barbershop.controller;
 
+import com.macpietr.barbershop.MailService;
+import com.macpietr.barbershop.UrlGenerator;
 import com.macpietr.barbershop.model.User;
 import com.macpietr.barbershop.service.BarberService;
 import com.macpietr.barbershop.service.UserService;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.mail.MessagingException;
+
 @Controller
 @AllArgsConstructor
 public class SignUpController {
 
     private UserService userService;
     private BarberService barberService;
+    private MailService mailService;
 
     @GetMapping("/signup")
     public String goToSignUpPage(Model model){
@@ -24,14 +29,27 @@ public class SignUpController {
     }
 
     @PostMapping("/signUp")
-    public String signUp(@ModelAttribute User user){
+    public String signUp(@ModelAttribute User user) throws MessagingException {
         if(userService.findUserByEmail(user.getEmail())==null
                 && barberService.findBarberByEmail(user.getEmail())==null){
             user.setRole("ROLE_USER");
+            user.setConfirmationUrl(UrlGenerator.generate());
+            user.setConfirmed(false);
             userService.saveUser(user);
-            return "redirect:/login";
+            //todo: remove it later
+            System.out.println("#############");
+            System.out.println("localhost:8080/confirmEmail?confirmationUrl="+user.getConfirmationUrl());
+            System.out.println("#############");
+            //todo: test if receiver exists
+            mailService.sendEmail(user.getEmail(),"Confirm your account","localhost:8080/confirmEmail?confirmationUrl="+user.getConfirmationUrl());
+            return "redirect:/confirmation";
         }else {
             return "redirect:/signup?error";
         }
+    }
+
+    @GetMapping("/confirmation")
+    public String goToConfirmationPage(){
+        return "confirmation";
     }
 }
