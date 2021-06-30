dbbd510efd3401f3303eb4f19f30d0a81d116f3f
macpietr <mac.pietruszewski@gmail.com>
2021-06-30 22:55:15 +0200

    add getmapping for reservation page

--- Webmodule/src/main/java/com/macpietr/barbershop/controller/ReservationController.java
+++ Webmodule/src/main/java/com/macpietr/barbershop/controller/ReservationController.java
@@ -1,10 +1,18 @@
 package com.macpietr.barbershop.controller;
 
 import com.macpietr.barbershop.service.ReservationService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class ReservationController {
 
     private ReservationService reservationService;
+
+    @GetMapping({"/reservation","/reservation.html"})
+    public String reservationPage(){
+        return "reservation";
+    }
 }
