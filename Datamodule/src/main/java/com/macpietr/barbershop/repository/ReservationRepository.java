aa4ebbc15cb41dd009aedd6f7728ef78c4f31c3b
macpietr <mac.pietruszewski@gmail.com>
2021-06-30 22:42:38 +0200

    add findAllByUser

--- Datamodule/src/main/java/com/macpietr/barbershop/repository/ReservationRepository.java
+++ Datamodule/src/main/java/com/macpietr/barbershop/repository/ReservationRepository.java
@@ -1,9 +1,13 @@
 package com.macpietr.barbershop.repository;
 
 import com.macpietr.barbershop.model.Reservation;
+import com.macpietr.barbershop.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    List<Reservation> findAllByUser(User user);
 }
